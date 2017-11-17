c9b99d78eb1c0b0368e6e558892b7b6304666e91
wenming <git@github.com>
2017-11-17 17:54:34 +0800

    git

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -21,16 +21,16 @@ class Index extends Base_1
 
     //后台首页
     public function home(){
-        // echo $_SERVER['REMOTE_ADDR'];
+        // echo $_SERVER['REMOTE_ADDR'];exit;
         $first_time = date('Y-m-d',strtotime('-1 months'));
         $second_time = date('Y-m-d');
         $year_time = date('Y-m-d',strtotime('-1 years'));
         //30天内访问人次
-        $person_time = Db::name('count')->whereTime('create_time','between',["{$first_time}","{$second_time}"])->count();
+        $person_time = Db::name('ip')->whereTime('create_time','between',["{$first_time}","{$second_time}"])->count();
         //1年内访问人次
-        $person_time_year = Db::name('count')->whereTime('create_time','between',["{$year_time}","{$second_time}"])->count();
+        $person_time_year = Db::name('ip')->whereTime('create_time','between',["{$year_time}","{$second_time}"])->count();
         //历史访问人数
-        $person_num = Db::name('count_person')->count();
+        $person_num = Db::name('ip')->count();
 
 
         return view('home',['meta_title'=>'后台首页','person_time'=>$person_time,'person_time_year'=>$person_time_year,'person_num'=>$person_num]);
